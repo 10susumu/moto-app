fdb788be743394122a92eb74a55a440ebbd75c32
10susumu <s.i.19921029@gmail.com>
2020-09-11 21:17:01 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   def configre_permitted_parameters
     added_attrs = [ :nickname, :avatar, :profile ]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs )
     devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs )
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs )
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
