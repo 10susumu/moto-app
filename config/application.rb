95630d39145eaa0ae3d6f28ad4ef1f8b1a3f76a5
10susumu <s.i.19921029@gmail.com>
2023-04-26 19:58:49 +0900

    rails require

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,16 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_cable/engine"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,7 +19,7 @@ Bundler.require(*Rails.groups)
 module MotoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    ?config.load_defaults 6.0
     config.assets.initialize_on_precompile = false
 
 
